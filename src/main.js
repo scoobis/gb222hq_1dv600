f3308fbe9fefaecb7bf2fed8198c2aceca211643
Gabriel Björlin <gabriel.bjorlin@gmail.com>
2019-02-07 15:05:35 +0100

    start

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('hello')
+console.log('Welcome to the hangman project')
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`Input username: `, (name) => {
+    console.log(`Your username is ${name}`)
+    let userName = name
+    readline.close()
+  })
